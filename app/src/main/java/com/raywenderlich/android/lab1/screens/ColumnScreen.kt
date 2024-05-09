ab161c064e5b9dcc2d80b017d9405765cdde9b9e
Alino44kaa <greenfox4488@gmail.com>
2024-05-09 23:37:51 +0300

    Контейнер Column

--- app/src/main/java/com/raywenderlich/android/lab1/screens/ColumnScreen.kt
+++ app/src/main/java/com/raywenderlich/android/lab1/screens/ColumnScreen.kt
@@ -25,5 +25,16 @@ fun ColumnScreen() {
 
 @Composable
 fun MyColumn(){
-
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.SpaceEvenly,
+        modifier = Modifier.fillMaxSize()
+    ) {
+        THREE_ELEMENT_LIST.forEach { textResId ->
+            Text(
+                text = stringResource(id = textResId),
+                fontSize = 22.sp
+            )
+        }
+    }
 }
